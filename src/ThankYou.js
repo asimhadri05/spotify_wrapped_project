e632ca325328e8292412330276cd261d2786d32e
Aditya <asimhadri3@gatech.edu>
2024-12-02 04:55:12 -0500

    changed all endpoints

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -51,7 +51,7 @@ const ThankYou = () => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/save-wrapped', {
+            const response = await fetch('https://spotify-wrapped-project.onrender.com/api/save-wrapped', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
